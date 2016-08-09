198c0eb3440e17855bf7cebb42a17a40be2ef781
dimlose <dimlose@gmail.com>
2016-08-09 20:48:34 +0900

    add facebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,9 +2,28 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
 
-  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
-  config.omniauth :google_oauth2, "1088233755713-mgh6uqmfa93t30h6tviqdggik0b2vd65.apps.googleusercontent.com", "f-yPbHqhB33I4RVLjI5_QO4z"
-  #config.omniauth :facebook, "158971204532748", "5bc8b916118485b2b3ce7d56cbd8e05f"
+  #OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
+  #config.omniauth :google_oauth2, "1088233755713-mgh6uqmfa93t30h6tviqdggik0b2vd65.apps.googleusercontent.com", "f-yPbHqhB33I4RVLjI5_QO4z", scope: "email,profile,offline", prompt: "consent"
+  #config.omniauth :facebook, "158971204532748", "5bc8b916118485b2b3ce7d56cbd8e05f", scope: "email"
+
+  #Replace example.com with your own domain name
+   config.mailer_sender = 'dimlose@gmail.com'
+
+   require 'devise/orm/active_record'
+   config.case_insensitive_keys = [ :email ]
+   config.strip_whitespace_keys = [ :email ]
+   config.skip_session_storage = [:http_auth]
+   config.stretches = Rails.env.test? ? 1 : 10
+   config.reconfirmable = true
+   config.expire_all_remember_me_on_sign_out = true
+   config.password_length = 8..128
+   config.reset_password_within = 6.hours
+   config.sign_out_via = :delete
+
+   #Add your ID and secret here
+   #ID first, secret second
+   config.omniauth :google_oauth2, "1088233755713-mgh6uqmfa93t30h6tviqdggik0b2vd65.apps.googleusercontent.com", "f-yPbHqhB33I4RVLjI5_QO4z", scope: "email,profile,offline", prompt: "consent"
+   config.omniauth :facebook, "158971204532748", "5bc8b916118485b2b3ce7d56cbd8e05f", scope: "email"
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
